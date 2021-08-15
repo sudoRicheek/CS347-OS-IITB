c956682390ed3e45ec872223c4fbdbac52ca2dde
sudoRicheek <richeekdas2001@gmail.com>
2021-08-16 05:04:01 +0530

    op mode 1 done

--- Lab1/shell-code/my_shell.c
+++ Lab1/shell-code/my_shell.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -9,6 +8,14 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+#define MAX_BG_PROCS 64
+
+// Modes of operation in Part B
+#define SINGLE_BG 1
+#define SERIES_FG 2
+#define PARALL_BG 3
+
+pid_t bg_proc[MAX_BG_PROCS];
 
 /* Splits the string by space and returns the array of tokens
 *
@@ -45,14 +52,87 @@ char **tokenize(char *line)
 	return tokens;
 }
 
+void multi_handler_func(char* line) {
+	uint OP_MODE = 0;
+	if (strstr(line, "&&&"))
+		OP_MODE = 3;
+	else if (strstr(line, "&&"))
+		OP_MODE = 2;
+	else if (strchr(line, '&'))
+		OP_MODE = 1;
+	
+	if (OP_MODE == 0) {
+		printf("%s\n", "MULTI_HANDLER_FUNC: NOT A MULTI HANDLER CASE");
+		return;
+	}
+	
+	char *linesegment;
+	char **tokens;
+	if (OP_MODE == 1) { // SINGLE BACKGROUND
+		linesegment = strtok(line, "&");
+		tokens = tokenize(linesegment);
+
+		// Check if anything is present after '&'
+		linesegment = strtok(NULL, "&");
+		if (linesegment != NULL && linesegment[0] != '\n') {
+			printf("%s\n", "SHELL: Unknown args found after '&'");
+			return;
+		}
+
+		// Fork and do our usual business, the only difference being, 
+		// we don't wait for the child process to die in the parent
+		// process.
+		pid_t pid_bg_child = fork();
+		if (pid_bg_child < 0) {
+			perror("SHELL: FATAL FORK FAILED");
+		}
+		else if (pid_bg_child == 0) {
+			int p = execvp(tokens[0], tokens);
+			if (p == -1) {
+				perror("SHELL"); // we expect execvp to provide the correct error response!
+				_exit(1);
+			}
+		}
+		else {
+			// Add the child to the list of bg processes.
+			for (size_t i = 0; i < MAX_BG_PROCS; i++)
+				if (bg_proc[i] == -1) {
+					bg_proc[i] = pid_bg_child;
+					break;
+				}
+		}
+	}
+
+
+	for (size_t i = 0; tokens[i] != NULL; i++)
+	{
+		free(tokens[i]);
+	}
+	free(tokens);
+}
+
 int main(int argc, char *argv[])
 {
 	char line[MAX_INPUT_SIZE];
 	char **tokens;
-	int i;
+	for (size_t i = 0; i < MAX_BG_PROCS; i++)
+		bg_proc[i] = -1;
+	
 
 	while (1)
 	{
+		// Check if background processes have finished and update bg_proc list accordingly
+		for (size_t i = 0; i < MAX_BG_PROCS; i++) {
+			if (bg_proc[i] != -1) {
+				pid_t child_proc = waitpid(bg_proc[i], NULL, WNOHANG);
+				if (child_proc == bg_proc[i]) { // meaning, bg_proc[i] has changed state 
+					bg_proc[i] = -1;
+					printf("%s%d %s\n", "SHELL: BACKGROUND PROCESS PID:", child_proc, "FINISHED");
+				}	
+			}
+		}
+		
+
 		/* BEGIN: TAKING INPUT */
 		bzero(line, sizeof(line));
 		printf("$ ");
@@ -77,10 +157,12 @@ int main(int argc, char *argv[])
 		// {
 		// 	printf("found token %s (remove this debug output later)\n", tokens[i]);
 		// }
-		// tokens is null termianted
+		// tokens is null terminated
 
-		
-		if (!strcmp(tokens[0],"cd")) {
+
+		// CASES: We can currently handle:
+
+		if (!strcmp(tokens[0], "cd")) {
 			if (tokens[1] == NULL || tokens[2] != NULL) {
 				printf("SHELL: Incorrect command\n");
 			}
@@ -91,7 +173,12 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
+		else if (strchr(line, '&')) { // Check if background execution is required
+			// handles execution of multiple commands at the same time
+			multi_handler_func(line); 
+		}
 		else {
+			// plain foreground execution case
 			pid_t pidchild = fork();
 
 			// printf("%d\n",pidchild);
@@ -116,7 +203,7 @@ int main(int argc, char *argv[])
 		
 
 		// Freeing the allocated memory
-		for (i = 0; tokens[i] != NULL; i++)
+		for (size_t i = 0; tokens[i] != NULL; i++)
 		{
 			free(tokens[i]);
 		}
