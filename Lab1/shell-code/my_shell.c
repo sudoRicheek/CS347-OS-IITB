91236e26a485df51eef8b4a95eef870931d6ed64
sudoRicheek <richeekdas2001@gmail.com>
2021-08-16 13:01:10 +0530

    part b done

--- Lab1/shell-code/my_shell.c
+++ Lab1/shell-code/my_shell.c
@@ -8,14 +8,18 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+
+// SHELL Limitations
 #define MAX_BG_PROCS 64
+#define MAX_FG_PROCS 64
 
 // Modes of operation in Part B
 #define SINGLE_BG 1
 #define SERIES_FG 2
-#define PARALL_BG 3
+#define PARALL_FG 3
 
 pid_t bg_proc[MAX_BG_PROCS];
+pid_t fg_proc[MAX_FG_PROCS];
 
 /* Splits the string by space and returns the array of tokens
 *
@@ -28,7 +32,6 @@ char **tokenize(char *line)
 
 	for (i = 0; i < strlen(line); i++)
 	{
-
 		char readChar = line[i];
 
 		if (readChar == ' ' || readChar == '\n' || readChar == '\t')
@@ -55,20 +58,21 @@ char **tokenize(char *line)
 void multi_handler_func(char* line) {
 	uint OP_MODE = 0;
 	if (strstr(line, "&&&"))
-		OP_MODE = 3;
+		OP_MODE = PARALL_FG;
 	else if (strstr(line, "&&"))
-		OP_MODE = 2;
+		OP_MODE = SERIES_FG;
 	else if (strchr(line, '&'))
-		OP_MODE = 1;
+		OP_MODE = SINGLE_BG;
 	
 	if (OP_MODE == 0) {
 		printf("%s\n", "MULTI_HANDLER_FUNC: NOT A MULTI HANDLER CASE");
 		return;
 	}
 	
-	char *linesegment;
-	char **tokens;
-	if (OP_MODE == 1) { // SINGLE BACKGROUND
+	char *linesegment = NULL; // Stores delimited commands
+	char **tokens = NULL; // tokenized commands
+
+	if (OP_MODE == SINGLE_BG) { // SINGLE BACKGROUND
 		linesegment = strtok(line, "&");
 		tokens = tokenize(linesegment);
 
@@ -78,6 +82,8 @@ void multi_handler_func(char* line) {
 			printf("%s\n", "SHELL: Unknown args found after '&'");
 			return;
 		}
+		if (tokens[0] == NULL) // check for emptiness
+			return;
 
 		// Fork and do our usual business, the only difference being, 
 		// we don't wait for the child process to die in the parent
@@ -95,33 +101,124 @@ void multi_handler_func(char* line) {
 		}
 		else {
 			// Add the child to the list of bg processes.
-			for (size_t i = 0; i < MAX_BG_PROCS; i++)
+			for (size_t i = 0; i < MAX_BG_PROCS; i++) {
 				if (bg_proc[i] == -1) {
 					bg_proc[i] = pid_bg_child;
 					break;
 				}
+				else if (i == MAX_BG_PROCS - 1) {
+					printf("SHELL: MAX LIMIT FOR BACKGROUND PROCESSES REACHED");
+					waitpid(pid_bg_child, NULL, 0); // Cant facilitate background execution
+				}
+			}
 		}
 	}
+	else if (OP_MODE == SERIES_FG) { // SERIES FOREGROUND
+		linesegment = strtok(line, "&"); // separated by && 
+		while (linesegment != NULL && linesegment[0] != '\n')
+		{
+			tokens = tokenize(linesegment);
 
+			if (tokens[0] == NULL)
+				continue;
+			
 
-	for (size_t i = 0; tokens[i] != NULL; i++)
-	{
-		free(tokens[i]);
+			pid_t pid_fg_child = fork();
+			if (pid_fg_child < 0) {
+				perror("SHELL: FATAL FORK FAILED");
+			}
+			else if (pid_fg_child == 0) {
+				int p = execvp(tokens[0], tokens);
+				if (p == -1) {
+					perror("SHELL"); // we expect execvp to provide the correct error response!
+					_exit(1); // gracefully exit if execvp fails
+				}
+			}
+			else {
+				// In parent shell
+				pid_t pidchange = waitpid(pid_fg_child, NULL, 0); // wait for child to die
+				
+				// DEBUG output
+				if (pidchange == pid_fg_child)
+					printf("%s done\n", tokens[0]);
+			}
+			linesegment = strtok(NULL, "&"); // get next command in sequence
+		}	
 	}
-	free(tokens);
+	else if (OP_MODE == PARALL_FG) { // PARALLEL FOREGROUND
+		linesegment = strtok(line, "&"); // separated by '&&&'
+		while (linesegment != NULL && linesegment[0] != '\n') {
+			tokens = tokenize(linesegment);
+
+			if (tokens[0] == NULL)
+				continue;
+
+			pid_t pid_pll_fg_chld = fork();
+			if (pid_pll_fg_chld < 0) {
+				perror("SHELL: FATAL FORK FAILED");
+			}
+			else if (pid_pll_fg_chld == 0) {
+				int p = execvp(tokens[0], tokens);
+				if (p == -1) {
+					perror("SHELL"); // we expect execvp to provide the correct error response!
+					_exit(1); // gracefully exit if execvp fails
+				}
+			}
+			else {
+				// In parent process
+				for (size_t i = 0; i < MAX_FG_PROCS; i++) {
+					if (fg_proc[i] == -1) {
+						fg_proc[i] = pid_pll_fg_chld;
+						break;
+					}
+					else if (i == MAX_FG_PROCS - 1) {
+						printf("SHELL: MAX LIMIT FOR FOREGROUND PROCESSES REACHED\n");
+						printf("SHELL: Can't facilitate parallel execution\n");
+						waitpid(pid_pll_fg_chld, NULL, 0); // Cant facilitate parallel execution
+					}
+				}
+			}			
+			linesegment = strtok(NULL, "&");
+		}		
+
+		// Make all the runnable processes and finally
+		// wait for them to die all at once!
+		for (size_t i = 0; i < MAX_FG_PROCS; i++) {
+			if (fg_proc[i] != -1) {
+				pid_t pidchange = waitpid(fg_proc[i], NULL, 0);
+
+				// DEBUG output
+				if (pidchange == fg_proc[i]) {
+					fg_proc[i] = -1;
+					printf("PID:%d done\n", pidchange);
+				}				
+			}			
+		}
+	}
+	
+	// Free the space!
+	if (tokens != NULL) {
+		for (size_t i = 0; tokens[i] != NULL; i++) {
+			free(tokens[i]);
+		}
+		free(tokens);
+	}	
 }
 
 int main(int argc, char *argv[])
 {
 	char line[MAX_INPUT_SIZE];
-	char **tokens;
+	char **tokens =  NULL;
 	for (size_t i = 0; i < MAX_BG_PROCS; i++)
 		bg_proc[i] = -1;
+	
+	for (size_t i = 0; i < MAX_FG_PROCS; i++)
+		fg_proc[i] = -1;
 	
 
 	while (1)
 	{
-		// Check if background processes have finished and update bg_proc list accordingly
+		// Check if background processes have finished -> reap and update bg_proc list accordingly
 		for (size_t i = 0; i < MAX_BG_PROCS; i++) {
 			if (bg_proc[i] != -1) {
 				pid_t child_proc = waitpid(bg_proc[i], NULL, WNOHANG);
@@ -150,18 +247,8 @@ int main(int argc, char *argv[])
 			free(tokens);
 			continue;
 		}
-		
-		//do whatever you want with the commands, here we just print them
-
-		// for (i = 0; tokens[i] != NULL; i++)
-		// {
-		// 	printf("found token %s (remove this debug output later)\n", tokens[i]);
-		// }
-		// tokens is null terminated
-
 
 		// CASES: We can currently handle:
-
 		if (!strcmp(tokens[0], "cd")) {
 			if (tokens[1] == NULL || tokens[2] != NULL) {
 				printf("SHELL: Incorrect command\n");
@@ -203,11 +290,12 @@ int main(int argc, char *argv[])
 		
 
 		// Freeing the allocated memory
-		for (size_t i = 0; tokens[i] != NULL; i++)
-		{
-			free(tokens[i]);
-		}
-		free(tokens);
+		if (tokens != NULL) {
+			for (size_t i = 0; tokens[i] != NULL; i++) {
+				free(tokens[i]);
+			}
+			free(tokens);
+		}		
 	}
 	return 0;
 }
